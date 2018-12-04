4aa169c26675feb01f157854cd8a80cd7bcd51aa
BhpAlpha <bhp-project@hotmail.com>
2018-12-04 15:17:24 +0800

    initial commit

---
+++ Bhp.SmartContract.Framework/OpCodeAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Bhp.VM;
+using System;
+
+namespace Bhp.SmartContract.Framework
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Constructor)]
+    public class OpCodeAttribute : Attribute
+    {
+        public OpCode[] OpCodes { get; }
+
+        public OpCodeAttribute(params OpCode[] opcodes)
+        {
+            this.OpCodes = opcodes;
+        }
+    }
+}
