692824a0c019e761f977937112de02d140f47dee
SkyeBlack <296471989@qq.com>
2019-09-02 10:09:04 +0800

    Added new syscalls - GetInvocationCounter and GetNotifications

---
+++ Bhp.SmartContract.Framework/Services/Bhp/Notification.cs
@@ -0,0 +1,15 @@
+﻿namespace Bhp.SmartContract.Framework.Services.Bhp
+{
+    public class Notification : IApiInterface
+    {
+        /// <summary>
+        /// Sender script hash
+        /// </summary>
+        public readonly byte[] ScriptHash;
+
+        /// <summary>
+        /// Notification's state
+        /// </summary>
+        public readonly object State;
+    }
+}
